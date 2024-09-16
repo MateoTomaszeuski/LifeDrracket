a51aa01019868c71d92853292af53b11ec65fdb5
MateoTomaszeuski <mtomaszeuski@gmail.com>
2024-09-16 08:53:37 -0600

    get cells around

--- Life.rkt
+++ Life.rkt
@@ -2,24 +2,56 @@
 
 (define (make-cell x y alive) 
   (list (cell=coordinates x y) alive)) 
-(define (cell=coordinates x  y)(cons x y))
-(define (x-coordinate c) (car c))
-(define (y-coordinate c) (cdr (car c)))
 
+(define (cell=coordinates x  y)
+  (cons x y))
+(define (x-coordinate c) (car (car c)))
+(define (y-coordinate c) (cdr (car c)))
+(define (alive? c) (cdr c))
 (define cells '())
 
 (define (append lst1 lst2)
   (foldr cons lst2 lst1))
 
-(define c1(make-cell 1 1 #t))
-(define c2(make-cell 2 1 #f))
-
-(define (cell-existis cell-list c)
+(define (cell-exists cell-list c)
   (cond 
     [(member c cell-list) #t]
     [else #f]))
 
+(define (get-alive-cells-around cell-list c)
+  (define alive-cells '())
+  (define top-left     (make-cell (- (x-coordinate c) 1)  (+ (y-coordinate c) 1) #t))
+  (define top          (make-cell (x-coordinate c)        (+ (y-coordinate c) 1) #t))
+  (define top-right    (make-cell (+ (x-coordinate c) 1)  (+ (y-coordinate c) 1) #t))
+  (define left         (make-cell (- (x-coordinate c) 1)  (y-coordinate c) #t))
+  (define right        (make-cell (+ (x-coordinate c) 1)  (y-coordinate c) #t))
+  (define bottom-left  (make-cell (- (x-coordinate c) 1)  (- (y-coordinate c) 1) #t))
+  (define bottom       (make-cell (x-coordinate c)        (- (y-coordinate c) 1) #t))
+  (define bottom-right (make-cell (+ (x-coordinate c) 1)  (- (y-coordinate c) 1) #t))
+  
+  (when (cell-exists cell-list top-left)
+    (set! alive-cells (append alive-cells (list top-left))))
+  (when (cell-exists cell-list top)
+    (set! alive-cells (append alive-cells (list top))))
+  (when (cell-exists cell-list top-right)
+    (set! alive-cells (append alive-cells (list top-right))))
+  (when (cell-exists cell-list left)
+    (set! alive-cells (append alive-cells (list left))))
+  (when (cell-exists cell-list right)
+    (set! alive-cells (append alive-cells (list right))))
+  (when (cell-exists cell-list bottom-left)
+    (set! alive-cells (append alive-cells (list bottom-left))))
+  (when (cell-exists cell-list bottom)
+    (set! alive-cells (append alive-cells (list bottom))))
+  (when (cell-exists cell-list bottom-right)
+    (set! alive-cells (append alive-cells (list bottom-right))))
+  
+  alive-cells)
 
+
+(define c1(make-cell 1 1 #t))
+(define c2(make-cell 2 1 #f))
+(define c3(make-cell 2 2 #t))
 
 (set! cells (append cells (list c1)))
 (display cells)
@@ -28,9 +60,17 @@
 (display cells)
 (newline)
 
-(display(cell-existis cells c1))
+(display(cell-exists cells c1))
 (newline)
-(display(cell-existis cells (make-cell 10 10 #f)))
+(display(cell-exists cells (make-cell 10 10 #f)))
 (newline)
-(display(cell-existis cells c2))
+(display(cell-exists cells c2))
 (newline)
+(display(cell-exists cells (make-cell 1 1 #f)))
+(newline)
+
+(set! cells (append cells (list c3)))
+
+(display(get-alive-cells-around cells c1))
+(newline)
+
